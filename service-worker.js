3d5c053f7282e6d59279c7d38260e0f0d9abb640
Tiana Husted <tiana.husted@gmail.com>
2021-03-27 14:40:17 -0700

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/InteractiveArtPortfolio/precache-manifest.63054860a3aef228b51d285d22100a0b.js"
+  "/thusted/InteractiveArtPortfolio/precache-manifest.7ff97d5266b7002c1036aec6033c0fae.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/InteractiveArtPortfolio/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/thusted/InteractiveArtPortfolio/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
